82344ed2973981a8cb52a82c9f2dc57c033a277d
Ubeyde Huzeyfe ARI <ubeydehuzeyfeari@gmail.com>
2022-07-05 23:13:03 +0100

    add initial files

---
+++ src/main/java/com/ubeydehuzeyfeari/Javafullstackdeveloper3Application.java
@@ -0,0 +1,13 @@
+package com.ubeydehuzeyfeari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javafullstackdeveloper3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javafullstackdeveloper3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubeydehuzeyfeari/Javafullstackdeveloper3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubeydehuzeyfeari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javafullstackdeveloper3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
